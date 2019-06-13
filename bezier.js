6477bdc05e0dbc92e3e1c64a695d9c45f33f281c
rodrigoap2 <rap2@cin.ufpe.br>
2019-06-13 00:11:28 -0300

    Implementação da funcionalidade de remoção de pontos

--- bezier.js
+++ bezier.js
@@ -1,11 +1,31 @@
 //Comando define se é pra remover(0), adicionar(1) ou Escolher(2) e 
 var comando = 0;
+var raioCirculo = 10;
 var cor = 'blue';
 var pontosAtuais = [];
 var allPoints = [];
 function mostrarPontos(){
 	allPoints.push(pontosAtuais);
 	allPoints.forEach(conj => conj.forEach(circ => circ.addTo(stage)));
+}
+
+function deletarPonto(pointx,pointy){
+	allPoints.forEach(conj => conj.forEach(function (circ){
+		if(verificarPonto(circ,pointx,pointy)){
+			var index = conj.indexOf(circ);
+			conj.splice(index, 1);
+			circ.clear();
+		}
+	}));
+	mostrarPontos();
+}
+
+function verificarPonto(circ,pointx,pointy){
+	if((circ.closePath()._attributes.x <= pointx+raioCirculo) && (circ.closePath()._attributes.x >= pointx-raioCirculo) && (circ.closePath()._attributes.y <= pointy+raioCirculo) && (circ.closePath()._attributes.y >= pointy-raioCirculo)){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 stage.on('message:adicionarCurva', function(){
@@ -20,12 +40,9 @@ stage.on('click', function(point){
 	if(comando === 1){
 		pontosAtuais = [];
    		comando = 3;
-   		console.log("aaaaa");
 	}else if(comando === 0){
 		var p = new Circle(point.x, point.y, 10).fill(cor);
-		console.log(p.points());
-		console.log('aaaaa');
-		//deletarPonto(point.x,point.y);
+		deletarPonto(point.x,point.y);
 	}else if(comando === 3){
 		var p = new Circle(point.x, point.y, 10).fill(cor);
    		pontosAtuais.push(p);
